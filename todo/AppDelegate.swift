96258ab140c0e92503e0580fc0a8c1ece8a974de
Taichi <taichi@asaichi.co.jp>
2018-10-06 06:57:05 +0900

    [fix] googlemapsのAPIキーの場所がおかしかったのを修正

--- todo/AppDelegate.swift
+++ todo/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
         GMSServices.provideAPIKey("")
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
